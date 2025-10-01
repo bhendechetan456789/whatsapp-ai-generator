87df0abb0d69f7b481f3d14f9bcb8feda75d8a52
Chetan Bhende <bhendechetan4@gmail.com>
2025-10-01 17:34:43 +0530

    commiT

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public")); 
 
-// Serve index.html on root
-app.get("/", (req, res) => {
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.post("/generate", (req, res) => {
   const { prompt } = req.body;
-
   const templates = [
     `Hello {name}, ${prompt}! Wishing you a wonderful day with your family. Namaste!`,
     `Hi {name}, ${prompt}! Hope you have a joyful and memorable celebration. Namaste!`,
@@ -26,6 +27,4 @@ app.post("/generate", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
